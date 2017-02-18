a292017cc0be1d165c055fd1e9002aa41e579d71
Yoland Gao <yoland5286@gmail.com>
2017-02-18 14:37:37 -0500

    Backend for registration

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
+from splinter import Browser
 from flask import Flask, request
 from flask import send_from_directory
 
 app = Flask(__name__)
+flaskBrowser = Flask(__name__)
 
 @app.route("/api/register", methods=['POST'])
 def register():
@@ -12,7 +14,23 @@ def register():
     password = json["password"]
 
 
+@app.route("/api/schoolCredentialCheck", methods=["POST"])
+def check_login():
+    json = request.form
+    studentId = json["studentId"]
+    password = json["password"]
 
+    with Browser('phantomjs') as browser:
+        url = 'https://studentvue.vbcps.com/Login_Student_PXP.aspx?'
+        browser.visit(url)
+
+        browser.find_by_id('username').fill(studentId)
+        browser.find_by_id('password').fill(password)
+        browser.find_by_id('Submit1').click()
+
+        if browser.url == 'https://studentvue.vbcps.com/Home_PXP.aspx':
+            return "OK"
+    return "Error"
 
 @app.route("/")
 def index():
