666155f5fab1e2de27827154c5211976f36eae19
Yoland Gao <yoland5286@gmail.com>
2017-02-28 12:07:36 -0500

    Update studentvue.py

--- igc/util/studentvue.py
+++ igc/util/studentvue.py
@@ -3,14 +3,14 @@ from splinter import Browser
 
 def check_authentication(studentId, password):
     browser  = Browser('phantomjs')
-    url = 'https://studentvue.vbcps.com/Login_Student_PXP.aspx'
+    url = 'https://parentvue.vbcps.com/Login_Student_PXP.aspx'
     browser.visit(url)
 
     browser.find_by_id('username').fill(studentId)
     browser.find_by_id('password').fill(password)
     browser.find_by_id('Submit1').click()
 
-    if browser.url == 'https://studentvue.vbcps.com/Home_PXP.aspx':
+    if browser.url == 'https://parentvue.vbcps.com/Home_PXP.aspx':
         return True, browser
     else:
         return False, browser
