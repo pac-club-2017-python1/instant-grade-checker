169205fac2f52fdcec5feed1a914635f33cee060
Yoland Gao <yoland5286@gmail.com>
2017-03-13 18:24:27 -0400

    Tweaked buttons

--- igc/controller/dashboard_controller.py
+++ igc/controller/dashboard_controller.py
@@ -16,10 +16,9 @@ def controller(app, models, db):
             user = session.query(User).filter(User.token == token).first()
             if user and int(user.student_id) in auth_controller.user_keys:
                 string = fileio.read("static/dashboard.html")
-                # password = auth_controller.user_keys[user.student_id]
 
                 if students[user.student_id]["table_body"] is None:
-                    cacheStudentData(students[user.student_id])
+                    cacheStudentData(user.student_id, students[user.student_id])
 
                 cache = students[user.student_id]
                 string = string.replace("{full_name}", cache["full_name"])
