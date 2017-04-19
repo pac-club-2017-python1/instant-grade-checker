4716e9f4321544cde53b92d47620b42449720478
Yoland Gao <yoland5286@gmail.com>
2017-04-19 19:52:06 -0400

    Server config:

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 from igc.controller.controller_register import register_controllers
 from igc.util import cache
 
-app = Flask(__name__, static_url_path='html')
+app = Flask(__name__, static_url_path='/html')
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('database_uri', 'sqlite:///./sqllite.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -16,11 +16,11 @@ register_controllers(app)
 
 @app.route("/")
 def index():
-    return app.send_static_file('index.html')
+    return app.send_static_file('/html/index.html')
 
 @app.route("/<path:path>")
 def send_static(path):
-    return send_from_directory('html', path)
+    return send_from_directory('/html', path)
 
 if __name__ == '__main__':
     thread = cache.CacheThread()
