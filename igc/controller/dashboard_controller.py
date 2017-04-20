f0e006ff68f9cef2378ef96f874c95ab47168cf6
Yoland Gao <yoland5286@gmail.com>
2017-04-19 22:45:28 -0400

    Debug

--- igc/controller/dashboard_controller.py
+++ igc/controller/dashboard_controller.py
@@ -167,6 +167,7 @@ def controller(app, models, db):
             if success:
                 user = session.query(User).filter(User.fid == target).first()
                 print str(auth_controller.user_keys)
+                print user, int(user.student_id) in auth_controller.user_keys
                 if user and int(user.student_id) in auth_controller.user_keys:
                     tokengen = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
                     tokengen = user.student_id + "_" + tokengen
